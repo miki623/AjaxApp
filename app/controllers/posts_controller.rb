9573de4e997f208ed610a29ec0deaa2aa4031dc9
miki623 <68592086+miki623@users.noreply.github.com>
2020-08-09 14:56:26 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,18 @@ class PostsController < ApplicationController
     redirect_to action: :index
   end
 
+  def checked
+    post = Post.find(params[:id])
+    if post.checked 
+      post.update(checked: false) #checkedカラムがboolean型なので、条件分岐で真偽値を判定
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: {post: item}
+
+  end
 
 
 end
